4c07e0e31dff44f8b0e39e69d0a2287100598155
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-13 14:21:43 +0100

    Add : Student class, with attributes, constructor and getter

--- src/org/lessons/java/valutazioni/Student.java
+++ src/org/lessons/java/valutazioni/Student.java
@@ -1,2 +1,33 @@
-package org.lessons.java.valutazioni;public class Student {
+package org.lessons.java.valutazioni;
+
+public class Student {
+    /* id dello studente (intero), percentuale assenze (intero), media voti (numero decimale)*/
+
+    // ----------- ATTRIBUTI --------------
+    private int id;
+    private int absence;
+    private double grades;
+
+    // ----------- COSTRUTTORI ----------------
+
+    public Student(int id, int absence, double grades) {
+        this.id = id;
+        this.absence = absence;
+        this.grades = grades;
+    }
+
+    // ----------- GETTER E SETTER ----------------
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAbsence() {
+        return absence;
+    }
+
+    public double getGrades() {
+        return grades;
+    }
 }
