3a10505f25f66105b77e96156bec1965675d9b6b
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-13 16:47:35 +0100

    Add : Course class and methods

---
+++ src/org/lessons/java/valutazioni/Course.java
@@ -0,0 +1,43 @@
+package org.lessons.java.valutazioni;
+
+import java.util.ArrayList;
+
+public class Course{
+    // Classe Corso che contiene una lista di studenti
+
+    // ----------- ATTRIBUTI ------------
+    private ArrayList<Student> students = new ArrayList<>();
+
+
+
+    // ----------- METODI ------------
+
+    // aggiungere uno studente alla lista
+    public void addStudent(ArrayList<Student> students, Student newStudent)  {
+        students.add(newStudent);
+    }
+
+    // remuovere uno studente dalla lista
+    public void removeStudent(ArrayList<Student> students,int id) {
+        students.remove(id-1);
+    }
+
+    // mostrare a video l’elenco di tutti gli studenti con i relativi dati
+    public void printStudents(ArrayList<Student> students) {
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+
+    // dare la percentuale di studenti promossi
+    public double promotedStudents(ArrayList<Student> students) {
+       int promotedStudents = 0;
+        for(Student student : students) {
+            if (student.isPromoted()) {
+                promotedStudents ++;
+            }
+        }
+        return (students.size() * ((double) promotedStudents / 100));
+
+    }
+}
--- src/org/lessons/java/valutazioni/Student.java
+++ src/org/lessons/java/valutazioni/Student.java
@@ -34,19 +34,19 @@ public class Student {
     // ----------- METODI ----------------
 
     // metodo che calcola, in base a percentuale assenze e media voti, se lo studente è promosso o bocciato
-    /* La valutazione complessiva viene fatta sulla percentuale di assenze (da 0 a 100) e la media dei voti (da 0 a 5):
-    - se lo studente ha fatto più del 50% di assenze è bocciato
-    - se ha fatto tra il 25% e il 50% di assenze è promosso solo se la media dei voti è superiore a 2
-    - se ha fatto meno del 25% di assenze è promosso se la media dei voti è superiore o uguale a 2 */
-    public String isPromoted (int absence, double grades) {
+    public boolean isPromoted () {
         if(absence > 50) {
-            return "The student is rejected :(";
+            return false;
         } else if((absence < 50 && absence > 25) && grades < 2) {
-            return "The student is rejected :(";
+            return false;
         } else if(absence < 25 && grades < 2) {
-            return "The student is rejected :(";
+            return false;
         }
-        return "The student is promoted :)";
+        return true;
     }
 
+    @Override
+    public String toString() {
+        return "Student " + id + ", absence = " + absence + ", grades = " + grades;
+    }
 }
